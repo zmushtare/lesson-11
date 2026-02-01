ad7e5d4db513b7999f99df80ccfdab0f81d31b8e
zmushtare <zmushtare@gmail.com>
2026-02-01 16:41:12 -0500

    hello world

--- scripts.js
+++ scripts.js
@@ -5,10 +5,15 @@ function startTime() {
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
+    let y = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let day = today.getDate();
+    console.log(today);
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
+    document.querySelector("#date").innerHTML = `${month}/${day}/${y}`;
 }
 
 function checkTime(i) {
